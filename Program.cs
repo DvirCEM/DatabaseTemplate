d602f12259ab3042bcff141d2eead19c21aa1ec0
dvirarazi0@gmail.com <dvirarazi0@gmail.com>
2025-07-13 17:39:39 +0300

    testing sync

--- Program.cs
+++ Program.cs
@@ -1,57 +1,14 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using DatabaseUtilities;
 using Microsoft.EntityFrameworkCore;
 
 class Program
 {
   static void Main()
   {
-    int port = 5000;
 
-    var server = new Server(port);
+    var newProduct = new Product("name", 0);
 
-    Console.WriteLine("The server is running");
-    Console.WriteLine($"Main Page: http://localhost:{port}/website/pages/index.html");
-
-    var database = new Database();
-
-    while (true)
-    {
-      var request = server.WaitForRequest();
-
-      Console.WriteLine($"Recieved a request: {request.Name}");
-
-      try
-      {
-        /*──────────────────────────────────╮
-        │ Handle your custome requests here │
-        ╰──────────────────────────────────*/
-        if (request.Name == "addProduct")
-        {
-          var (name, price) = request.GetParams<(string, double)>();
-
-          var newProduct = new Product(name, price);
-
-          database.Products.Add(newProduct);
-
-          database.SaveChanges();
-        }
-        else if (request.Name == "getProducts")
-        {
-          request.Respond(database.Products.ToArray());
-        }
-        else
-        {
-          request.SetStatusCode(405);
-        }
-      }
-      catch (Exception exception)
-      {
-        request.SetStatusCode(422);
-        Log.WriteException(exception);
-      }
-    }
   }
 }
 
@@ -61,15 +18,7 @@ class Database() : DbBase("database")
   /*──────────────────────────────╮
   │ Add your database tables here │
   ╰──────────────────────────────*/
-  public DbSet<User> Users { get; set; } = default!;
   public DbSet<Product> Products { get; set; } = default!;
-}
-
-class User(string id, string username, string password)
-{
-  [Key] public string Id { get; set; } = id;
-  public string Username { get; set; } = username;
-  public string Password { get; set; } = password;
 }
 
 class Product(string name, double price)
